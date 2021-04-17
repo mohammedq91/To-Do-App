1d7f6d9ba15d9bf53ae4542eb98c73c86df48232
Mohammed <mohammedq91@gmail.com>
2021-04-17 11:35:51 -0400

    Outlining steps in To-DO-App JS file

---
+++ script.js
@@ -0,0 +1,27 @@
+// input.onclick event to insert text inside input field
+
+
+//input.submit event to submit text
+
+
+//open list when input is submitted
+
+
+//enable cross item when completed
+
+
+//deleting item
+
+
+
+//clear list
+
+
+
+
+
+
+
+
+
+
