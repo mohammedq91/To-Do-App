9c209c4bda103b2053d7537e940818f1192ef991
Mohammed <mohammedq91@gmail.com>
2021-04-24 11:25:22 -0400

    Activate delete item button

--- script.js
+++ script.js
@@ -1,47 +1,48 @@
-//Establish variables and grab tags from HTML
-var input = document.getElementById('task');
-var sumbit = document.QuerySelector('#submit')
-var clear = document.getElementById('clear')
-console.log(task)
-console.log(submit)
-console.log(clear)
-
-
-// submit input
-
-input.onsubmit = function(addTask){
-    if (input.value != " "){
-        return input.value = addTask;
-        console.log(addTask);
-     input.value = ' '
-    }
-
-}
-addTask()
-console.log(addTask)
+const form = document.querySelector('#form');
+var clear = document.querySelector('#clear') 
+const input = document.getElementById('task')
+var list = document.getElementById('taskList');
+const li = document.createElement('li')
+const removeButton = document.getElementById('remove')
 
 
 
-//input.submit event to submit text
+//load submit button event when task is enterered
+form.addEventListener('click', function (e){
+    e.preventDefault();
+    
+    if (input.value == ""){
+       // window.alert("Please enter an item!")
+    } else {
+        //(input.value != "");
+        addTask(input.value);
+        // clearing input field after task submission
+    input.value = "";
+    };
+});
 
+function addTask(task){
+    // create new line
+    const li = document.createElement('li');
 
-//open list when input is submitted
+     // add task to the new line
+    //li.textContent = input.value 
 
+    li.innerHTML = '<button id="remove"> x </button>' + input.value
+         
+    // append the new line to the task-listS
+    list.appendChild(li);
+};
 
-//enable cross item when completed
+clear.addEventListener('click', function(){
+   list.innerHTML = "";
+});
 
+removeButton.onclick =function(){
+    //list.removeChild(li)
+    li.value =""
+    list.appendChild(li)
 
-//deleting item
+};
 
-
-//clear list
-
-
-
-
-
-
-
-
-
-
+// cross out task when completed
